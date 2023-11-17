f8d973fb9fbe4ca7120a13ffdadbf9cc03d56e3a
alleycat1 <netragonlong@gmail.com>
2023-11-18 02:56:45 +0900

    Supabase REST API for Godot version check

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # supabase_godot_version_api
 supabase rest api for godot version check
+
+Method for Deploy
+
+npm init
+npm i supabase --save-dev
+./node_modules/.bin/supabase projects list
+./node_modules/.bin/supabase init
+./node_modules/.bin/supabase link --project-ref <Project ID>
+./node_modules/.bin/supabase functions deploy <function name> --no-verify-jwt
---
+++ supabase/functions/godot_version_api/index.ts
@@ -0,0 +1,14 @@
+import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2.7.1'
+
+async function getVersion() {
+  const supabase = await createClient("https://lqdlzjvnpaybefftxuhh.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZGx6anZucGF5YmVmZnR4dWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAyMDQ5NDEsImV4cCI6MjAxNTc4MDk0MX0.avrMCaZPw0iHfgZqTA2nKwBLrmcx45TcV4X00W2rre0");
+  const { data, error } = await supabase.from('versions').select('*').order('id', { ascending: false }).limit(1);
+  return new Response(JSON.stringify({ data }), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 200,
+  })
+}
+
+Deno.serve(async (req) => {
+  return getVersion();
+})
